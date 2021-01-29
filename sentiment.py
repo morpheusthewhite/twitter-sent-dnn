976073ec9632848bf61f8b44bb563a52b4d9adac
morpheusthewhite <zffromGerace@hotmail.it>
2021-01-29 19:01:17 +0100

    style: remove trailing whitespace

--- sentiment.py
+++ sentiment.py
@@ -9,16 +9,16 @@ from cPickle import load
 def get_word_index_array(words, word2index):
     u"""
     Transform the words into list of int(word index)
-    
+
     Note: Unknown words are dropped
-    
+
     >>> words = [u"I", u"love", u"you", u"RANDOM STUFF"]
     >>> word2index = {u"I": 0, u"love": 1, u"you": 2}
     >>> get_word_index_array(words, word2index)
     [0, 1, 2]
     """
-    return [word2index[w] 
-            for w in words 
+    return [word2index[w]
+            for w in words
             if word2index.get(w) is not None # filter out those unknown
     ]
 
@@ -27,16 +27,16 @@ def pad_sents(sents, padding_token_index):
     """
 
     Pad the sents(in word index form) into same length so they can form a matrix
-    
+
     # 15447
     >>> sents = [[1,2,3], [1,2], [1,2,3,4,5]]
     >>> pad_sents(sents, padding_token_index = -1)
     [[1, 2, 3, -1, -1], [1, 2, -1, -1, -1], [1, 2, 3, 4, 5]]
     """
-    max_len_sent = max(sents, 
+    max_len_sent = max(sents,
                        key = lambda sent: len(sent))
     max_len = len(max_len_sent)
-    
+
     get_padding = lambda sent: [padding_token_index] * (max_len - len(sent))
     padded_sents = [(sent + get_padding(sent))
                     for sent in sents]
@@ -56,7 +56,7 @@ MODEL = DCNN(params)
 def sentiment_scores_of_sents(sents):
     """
     Predict the sentiment positive scores for a bunch of sentences
-    
+
     >>> sentiment_scores_of_sents([u'simultaneously heart breaking and very funny , the last kiss is really all about performances .', u'( u ) stupid .'])
     array([ 0.78528505,  0.0455901 ])
     """
@@ -64,7 +64,7 @@ def sentiment_scores_of_sents(sents):
                     for sent in sents]
 
     x = np.asarray(
-        pad_sents(word_indices, PADDING_INDEX), 
+        pad_sents(word_indices, PADDING_INDEX),
         dtype = np.int32
     )
 
